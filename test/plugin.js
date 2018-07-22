c982b56eecabff3aeb6be8d16898408365c6c006
Simon Laroche <hi@simon.lc>
2018-07-21 20:16:44 -0400

    Refactor code

--- test/plugin.js
+++ test/plugin.js
@@ -591,3 +591,96 @@ test('ignore classes', t => {
       t.is(result.html, expected);
     });
 });
+
+test('works with no options', t => {
+  const html = `
+  <html>
+    <style>
+      #some-id {
+        text-transform: uppercase;
+      }
+      .header__intro {
+        color: blue;
+      }
+      .card--profile {
+        background: white;
+      }
+      .js-overlay {
+        display: none;
+      }
+      #js-button {
+        color: blue;
+      }
+      @media (min-width: 768px) {
+        .header__intro {
+          color: gray;
+        }
+      }
+    </style>
+    <body>
+      <svg style="display:none">
+        <symbol id="icon-location"><path d=""></path></symbol>
+      </svg>
+      <h1 id="some-id">Title</h1>
+      <p class="header__intro">OMG</p>
+      <div class="js-overlay"></div>
+      <div id="js-button"></div>
+      <div class="card--profile">
+        card content
+      </div>
+      <svg>
+        <use xlink:href="#icon-location"></use>
+      </svg>
+      <label for="username">Click me</label>
+      <input type="text" id="username">
+    </body>
+  </html>
+  `;
+  const expected = `
+  <html>
+    <style>
+      #a {
+        text-transform: uppercase;
+      }
+      .a {
+        color: blue;
+      }
+      .b {
+        background: white;
+      }
+      .js-overlay {
+        display: none;
+      }
+      #js-button {
+        color: blue;
+      }
+      @media (min-width: 768px) {
+        .a {
+          color: gray;
+        }
+      }
+    </style>
+    <body>
+      <svg style="display:none">
+        <symbol id="b"><path d=""></path></symbol>
+      </svg>
+      <h1 id="a">Title</h1>
+      <p class="a">OMG</p>
+      <div class="js-overlay"></div>
+      <div id="js-button"></div>
+      <div class="b">
+        card content
+      </div>
+      <svg>
+        <use xlink:href="#b"></use>
+      </svg>
+      <label for="c">Click me</label>
+      <input type="text" id="c">
+    </body>
+  </html>
+  `;
+  return posthtml().use(plugin()).process(html)
+    .then(result => {
+      t.is(result.html, expected);
+    });
+});
